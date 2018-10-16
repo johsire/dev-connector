32a8bea38b9175e899288ec8360eeaf84b9b583b
Josiah Maina <josiah5115@gmail.com>
2018-10-16 10:24:30 -0600

    fixed db connections issues

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+ mongoURI: 'mongodb://devJoh:devJoh18@ds037478.mlab.com:37478/devsconnect'
+}
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB Config:
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoBD Using Mongoose;
+mongoose
+      .connect(db)
+      .then(() => console.log('MongoDB Connected'))
+      .catch(err => console.log(err, 'MongoDB Error!'))
 
 app.get('/', (req, res) => res.send('Hello Utah!'));
 
