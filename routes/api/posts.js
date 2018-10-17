ec1a64eb8ef4039508fc5b5b96ba4a5ae5e4c870
Josiah Maina <josiah5115@gmail.com>
2018-10-16 19:57:17 -0600

    comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,7 +4,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/test", (req, res) => res.json({ msg: "Posts Wroks!" })
-);
+// @route   GET: api/users/test
+// @desc    Tests users route
+// @access  Public
+router.get("/test", (req, res) => res.json({ msg: "Posts Works!" }));
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,11 +2,13 @@
 // create and fetch the users profile
     // (location, bio, experiences, education, social network links)
     // Profile model and User's model
-    
+
 const express = require("express");
 const router = express.Router();
 
-router.get("/test", (req, res) => res.json({msg: "Profile Works!"})
-);
+// @route   GET: api/profile/test
+// @desc    Tests profile route
+// @access  Public
+router.get("/test", (req, res) => res.json({ msg: "Profile Works!" }));
 
 module.exports = router;
