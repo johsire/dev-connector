5a0df094ddd32ea3d7cbfefda57a1c2fd9ebfab8
Josiah Maina <josiah5115@gmail.com>
2018-10-21 15:59:04 -0600

    login/ find user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,5 @@
 
-// authentication 
+// authentication
 // login(username, email)
 // passport
 
@@ -28,14 +28,14 @@ router.post('/register', (req, res) => {
          if (user) {
            return res.status(400).json({ email: "Email already exists!"});
          } else {
-          
+
           const avatar = gravatar.url(req.body.email, {
             // Size
             s: '200',
             // Rating
             r: 'pg',
             // Default
-            d: 'mm' 
+            d: 'mm'
           });
 
           const newUser = new User({
@@ -44,7 +44,7 @@ router.post('/register', (req, res) => {
             avatar,
             password: req.body.password
           });
-          
+
           bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(newUser.password, salt, (err, hash) => {
               if (err) throw err;
@@ -56,6 +56,34 @@ router.post('/register', (req, res) => {
           })
          }
       })
+});
+
+// @route   GET: api/users/login
+// @desc    Login users / Returning
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({email})
+    .then(user => {
+      // check for user 
+      if (user) {
+        return res.status(404).json({email: 'User not found!'})
+      }
+
+      // Check Password 
+      bcrypt.compare(password, user.password)
+          .then(isMatch => {
+            if(isMatch) {
+              res.json(msg: "Success!");
+            } else {
+              return res.status(400).json({password: 'Password incorrect'})
+            }
+          })
+    });
+
 });
 
 module.exports = router;
