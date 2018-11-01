a49150046502cf08641bea25f78d3d8af79ef31f
Josiah Maina <josiah5115@gmail.com>
2018-11-01 13:09:07 -0600

    private/ authorized user

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,6 +9,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 
 // Load User Model
@@ -101,7 +102,19 @@ router.post('/login', (req, res) => {
             }
           })
     });
+});
 
+// @route   GET: api/users/current
+// @desc    Return Current user 
+// @access  Private
+router.get(
+  '/current',
+   passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  })
 });
 
 module.exports = router;
