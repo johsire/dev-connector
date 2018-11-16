388c092d1127c4a3a07550db4e97b7f33ed2316c
Josiah Maina <josiah5115@gmail.com>
2018-11-15 19:11:42 -0700

    User Profile Validation

--- validation/profile.js
+++ validation/profile.js
@@ -1,6 +1,7 @@
 const Validator = require('validator');
 const isEmpty = require('./is-empty');
 
+// VALIDATION CODE //->>>----------------------------------->>>>
 module.exports = function validateProfileInput(data) {
 	let errors = {};
 
@@ -8,9 +9,58 @@ module.exports = function validateProfileInput(data) {
 	data.status = !isEmpty(data.status) ? data.status : '';
 	data.skills = !isEmpty(data.skills) ? data.skills : '';
 
-	if (!Validator.isLength(data.)) {
-		
+	if(!Validator.isLength(data.handle, { min: 2, max: 40 })) {
+		errors.handle = 'Handle needs to be 2 and 4 characters';
 	}
+
+	if(Validator.isEmpty(data.handle)) {
+		error.handle = 'Profile handle is required!';
+	}
+
+	if(Validator.isEmpty(data.handle)) {
+		errors.status = 'Status field is required';
+}
+
+	if(Validator.isEmpty(data.skills)) {
+		errors.skills = 'Skills field is required';
+	}
+
+	if (!isEmpty(data.website)) {
+		if (!Validator.isURL(data.website)) {
+			errors.website = 'Not a valid URL';
+		}
+	}
+
+	if (!isEmpty(data.youtube)) {
+		if (!Validator.isURL(data.youtube)) {
+			errors.youtube = 'Not a valid URL';
+		}
+	}
+
+	if (!isEmpty(data.twitter)) {
+		if (!Validator.isURL(data.twitter)) {
+			errors.twitter = 'Not a valid URL';
+		}
+	}
+
+	if (!isEmpty(data.facebook)) {
+		if (!Validator.isURL(data.facebook)) {
+			errors.facebook = 'Not a valid URL';
+		}
+	}
+
+	if (!isEmpty(data.linkedin)) {
+		if (!Validator.isURL(data.linkedin)) {
+			errors.linkedin = 'Not a valid URL';
+		}
+	}
+	
+	if (!isEmpty(data.instagram)) {
+		if (!Validator.isURL(data.instagram)) {
+			errors.instagram = 'Not a valid URL';
+		}
+	}
+
 	return {
 		errors,
 		isValid: isEmpty(errors),
