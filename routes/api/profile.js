51bdfbdcaae97d82fd914729823b82751ccfa615
Josiah Maina <josiah5115@gmail.com>
2018-11-19 23:23:08 -0700

    Get All Profiles & bug fixed

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -55,6 +55,28 @@ router.get(
 	}
 );
 
+// ---------------> GET/ Get All Profiles ------------------------------>
+// @route   GET api/profile/all
+// @desc    Get all profiles 
+// @access  Public
+
+router.get('/all', (req, res) => {
+	const errors = {};
+
+	Profile.find()
+		.populate('user', ['name', 'avatar'])
+		.then(profiles => {
+			if (!profiles) {
+				errors.noprofile = 'There are no profiles';
+				return res.status(404).json(errors);
+			}
+
+			res.json(profiles);
+		})
+		.catch(err => res.status(404).json({ profile: 'There is no profiles' }));
+});
+
+
 // ---------------> GET/ Get Profile by handle ------------------------------>
 // @route   GET: api/profile/handle/:handle
 // @desc    Get Profile by handle  
@@ -64,16 +86,16 @@ router.get('/handle/:handle', (req, res) => {
 	const errors = {};
 
 	Profile.findOne({ handle: req.params.handle })
-		.populate('user', ['name', 'avatar'])
-		.then(profile => {
-			if (!profile) {
-				errors.noprofile = 'There is no profile for this user';
-				res.status(404).json(errors);
-			}
-			
-			res.json(profile);
-		})
-		.catch(err => res.status(404).json(err));
+			.populate('user', ['name', 'avatar'])
+			.then(profile => {
+					if (!profile) {
+							errors.noprofile = 'There is no profile for this user';
+							res.status(404).json(errors);
+					}
+
+					res.json(profile);
+			})
+			.catch(err => res.status(404).json({ profile: 'There is no profile for this user' }));	
 });
 
 // ---------------> GET/ Get Profile by User ID ------------------------------>
@@ -85,16 +107,18 @@ router.get('/user/:user_id', (req, res) => {
 	const errors = {};
 
 	Profile.findOne({ user: req.params.user_id })
-		.populate('user', ['name', 'avatar'])
-		.then(profile => {
-			if (!profile) {
-				errors.noprofile = 'There is no profile for this user';
-				res.status(404).json(errors);
-			}
-			
-			res.json(profile);
-		})
-		.catch(err => res.status(404).json(err));
+			.populate('user', ['name', 'avatar'])
+			.then(profile => {
+					if (!profile) {
+							errors.noprofile = 'There is no profile for this user';
+							res.status(404).json(errors);
+					}
+
+					res.json(profile);
+			})
+			.catch(err =>
+					res.status(404).json({ profile: 'There is no profile for this user' })
+			);
 });
 
 // ---------------> POST/ Create or Edit New User Profile --------------------->
